75ba15843e056fefd1ada4db11c5df66e057fa31
rhuanwinck-blip <rhuanwinck@gmail.com>
2026-04-27 22:35:15 -0300

    salvo

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Veltrix AI carregado com sucesso.");
+
+document.querySelectorAll(".btn-primary").forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log("Clique em botão de assinatura");
+  });
+});
